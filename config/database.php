13dd7fbce2de20812067c1caeaae2ba115611fd8
Sebastian Alfers <Sebastian Alfers>
2011-05-21 19:05:38 +0200

    config for test server

--- config/database.php
+++ config/database.php
@@ -1,25 +1,51 @@
 <?php
-class DATABASE_CONFIG {
 
-	var $default = array(
-		'driver' => 'mysql',
-		'persistent' => false,
-		'host' => '127.0.0.1',
-		'port' => '',
-		'login' => 'root',
-		'password' => 'root',
-		'database' => 'myzeitung',
+class DATABASE_CONFIG
+{
+	//initalize variable as null
+	var $default=null;
+
+	//set up connection details to use in Live production server
+	var $test =
+		array(
+            'driver' => 'mysql',
+            'persistent' => false,
+            'host' => 'mysql5.on-line-solutions.de',
+            'port' => '',
+            'login' => 'db214998_8',
+            'password' => 'databa$e',
+            'database' => 'db214998_8',
+		);
+
+	// and details to use on your local machine for testing and development
+	var $dev = array(
+            'driver' => 'mysql',
+            'persistent' => false,
+            'host' => '127.0.0.1',
+            'port' => '',
+            'login' => 'root',
+            'password' => 'root',
+            'database' => 'myzeitung',
 	);
-	
-	var $test = array(
-		'driver' => 'mysql',
-		'persistent' => false,
-		'host' => '127.0.0.1',
-		'port' => '',
-		'login' => 'root',
-		'password' => 'root',
-		'database' => 'myzeitung',
-		'prefix' => ''
-	);	
+
+
+	// the construct function is called automatically, and chooses prod or dev. UPdate! works for baking now
+	function __construct ()
+	{
+		//check to see if server name is set (thanks Frank)
+		if(isset($_SERVER['SERVER_NAME'])){
+			switch($_SERVER['SERVER_NAME']){
+				case 'myzeitung.loc':
+					$this->default = $this->dev;
+					break;
+				case 'on-line-solutions.de':
+					$this->default = $this->test;
+					break;
+			}
+		}
+	    else // we are likely baking, use our local db
+	    {
+	        $this->default = $this->dev;
+	    }
+	}
 }
-?>
