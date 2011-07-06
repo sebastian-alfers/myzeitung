9aa88135296abc58ea86079b4bd400102eb4ecc5
Tim <ecuted@gmail.com>
2011-07-06 20:04:54 +0200

    merge

--- app/models/solr.php
+++ app/models/solr.php
@@ -19,7 +19,7 @@ class Solr extends AppModel {
 	const SEARCH_RESULT_SEARCH_FIELD_AUTO_SUGGEST = 'search_field_auto_suggest';
 	
 	const HOST = 'localhost';
-	const PORT = 8080;
+	const PORT = 8983;
 	const PATH = '/solr';
 
 
