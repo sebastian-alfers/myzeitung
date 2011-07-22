4663019bb7662723fb407b9bfae1c1846538e788
Sebastian Alfers <Sebastian Alfers>
2011-07-22 14:44:09 +0200

    redirect handling

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -15,6 +15,12 @@ class UsersController extends AppController {
 
 
 	public function login(){
+        //check, if the user is already logged in
+        if($this->Session->read('Auth.User.id')){
+            //redirct to his profile
+            $this->redirect(array('controller' => 'users', 'action' => 'view'));
+        }
+
 		// login with username or email
 		// the following code is just for the case that the combination of user.username(!) and user.password did not work:
 		//	trying the combination of user.email and user.password
@@ -718,6 +724,12 @@ class UsersController extends AppController {
 	 }
 	 */
     function forgotPassword() {
+        //check, if the user is already logged in
+        if($this->Session->read('Auth.User.id')){
+            //redirct to his profile
+            $this->redirect(array('controller' => 'users', 'action' => 'view'));
+        }
+
       if(!empty($this->data)) {
         $this->User->contain();
         $user = $this->User->findByEmail($this->data['User']['email']);
