bc8d7e95b4f2c14ecb39d29cffc15543e0d40380
ecuted <ecuted@gmail.com>
2011-09-29 18:28:53 +0200

    changed user description to 1000 characters

---
+++ app/install/users/users_0.2.8.php
@@ -0,0 +1,15 @@
+<?php
+
+$sql[] = "ALTER TABLE `users` CHANGE `description` `description` VARCHAR(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL";
+
+//**** !2nd param is IMPORTANT! ****
+$log = 'description 1000 letters';
+
+
+
+
+?>
+
+
+
+
