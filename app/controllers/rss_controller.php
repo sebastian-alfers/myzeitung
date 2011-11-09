5cbbac90010dbf25378ba7c7e93817d74ca858a1
Sebastian Alfers <sebastian.alfers@gmail.com>
2011-11-09 13:04:59 +0100

    added simplepe lib and controller

---
+++ app/controllers/rss_controller.php
@@ -0,0 +1,57 @@
+<?php
+
+require "libs/Simplepie/simplepie.inc";
+
+class RssController extends AppController {
+
+	var $name = 'Rssimport';
+
+	var $uses = array();
+
+    var $cache;
+
+      function __construct() {
+        parent::__construct();
+        $this->cache = CACHE . 'rss' . DS;
+      }
+
+	public function beforeFilter(){
+		parent::beforeFilter();
+		$this->Auth->allow('import');
+	}
+
+    function import(){
+        debug($this->cache);
+    //make the cache dir if it doesn't exist
+    if (!file_exists($this->cache)) {
+        if (!mkdir($this->cache, 0755, true)) {
+            $this->log('can not create directory for post: ' . $this->cache);
+            return false;
+        }
+    }
+
+//setup SimplePie
+    $feed = new SimplePie();
+    $feed->set_feed_url('http://www.facebook.com/feeds/page.php?id=271834416189258&format=rss20');
+
+
+    $feed->set_cache_location($this->cache);
+
+    //retrieve the feed
+    $feed->init();
+
+    //get the feed items
+    $items = $feed->get_items();
+
+    //return
+    if ($items) {
+      foreach ($feed->get_items() as $item){
+          echo ($item->get_title()) . ' ' . $item->get_permalink();
+          #http://simplepie.org/wiki/setup/sample_page
+      }
+    } else {
+      return false;
+    }
+    }
+
+}
