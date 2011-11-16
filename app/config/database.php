b50bf229d8e7873a310e3ccf9590a2c0f6f96287
Sebastian Alfers <sebastian.alfers@gmail.com>
2011-11-16 11:47:45 +0100

    changed db config

--- app/config/database.php
+++ app/config/database.php
@@ -16,7 +16,7 @@ class DATABASE_CONFIG
   */
 
 	// local
-	var $local = array(
+	var $default = array(
             'driver' => 'mysql',
             'persistent' => false,
             'host' => '127.0.0.1',
@@ -27,12 +27,4 @@ class DATABASE_CONFIG
 	);
 
 
-
-
-	// the construct function is called automatically, and chooses prod or dev. UPdate! works for baking now
-	function __construct ()
-	{
-        $this->default = $this->local;
-
-	}
 }
